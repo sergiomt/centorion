185062c2bff968f07ac5850be88f6b8a3539633b
Sergio Montoro <sergiom@knowgate.com>
2017-06-09 11:18:14 +0100

    Remove unnecessary code

--- vagrant-setup/java80.sh
+++ vagrant-setup/java80.sh
@@ -14,13 +14,12 @@ else
 	source /vagrant/vagrant-setup/include.sh
 
 	# Install Java 8
-	# RPM=jdk-8u5-linux-x64.rpm
-	# JDK=otn-pub/java/jdk/8u5-b13
 	RPM=jdk-8u131-linux-x64.rpm
-	JDK=otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163
+	OTN=otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163
 	if [ ! -f "$SETUP/cache/jdk-8u5-linux-x64.rpm" ]
 		then
-		wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/$JDK/$RPM" -P $SETUP/cache
+		echo "Downloading JDK 8.0"
+		wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/$OTN/$RPM" -P $SETUP/cache
 	fi
 	rpm -Uvh $SETUP/cache/$RPM
 
@@ -29,27 +28,35 @@ else
 	cd $JAVA
 	if [ ! -f "$SETUP/cache/jai-1_1_3-lib-linux-amd64-jdk.bin" ]
 		then
+		echo "Downloading JAI 1.1.3"
 		wget http://download.java.net/media/jai/builds/release/1_1_3/jai-1_1_3-lib-linux-amd64-jdk.bin -P $SETUP/cache
 		chmod u+x -P $SETUP/cache/jai-1_1_3-lib-linux-amd64-jdk.bin
 	fi
 	cd $JDK
+	echo "Installing JAI 1.1.3"
 	echo -e "y\n" | $SETUP/cache/jai-1_1_3-lib-linux-amd64-jdk.bin
 
 	cd /etc/alternatives
-	unlink jre
-	unlink jre_exports
+	if [ -L "jre" ]
+	then
+		unlink jre > /dev/null 2>&1
+	fi
+	if [ -L "jre" ]
+	then
+		unlink jre_exports > /dev/null 2>&1
+	fi
 	ln -s $JDK/jre jre
 
-	BINS=( java keytool orbd pack200 rmid rmiregistry servertool tnameserv unpack200 )
-
-	for NM in "${BINS[@]}"
-	do
-		if [ -h "$NM" ]
-		then
-			unlink $NM.1.gz > /dev/null 2>&1
-		fi
-		ln -s $JDK/bin/$NM $NM
-	done
+	# Required only if there was a previous Java 7 installation that isbeing replaced
+	# BINS=( java keytool orbd pack200 rmid rmiregistry servertool tnameserv unpack200 )
+	# for NM in "${BINS[@]}"
+	# do
+	# 	if [ -L "$NM" ]
+	# 	then
+	# 		unlink $NM.1.gz > /dev/null 2>&1
+	# 	fi
+	# 	ln -s $JDK/bin/$NM $NM
+	# done
 
 	cd $PPWD
 
