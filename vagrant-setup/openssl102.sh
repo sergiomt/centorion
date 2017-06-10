125497facf7f696cd5f75043e06d39322ef59afd
Sergio Montoro <sergiom@knowgate.com>
2017-06-10 07:50:26 +0100

    Install OpenSSL 1.0.2 instead of 1.0.1

---
+++ vagrant-setup/openssl102.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PPWD=$PWD
+
+SETUP="/vagrant/vagrant-setup"
+
+source $SETUP/include.sh
+
+cd /usr/local/src
+wget_and_untar  https://www.openssl.org/source/ openssl-1.0.2-latest.tar.gz
+cd openssl-1.0.2l
+./config
+make
+make install
+
+rm /usr/bin/openssl
+
+ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
+
+cd $PWD
