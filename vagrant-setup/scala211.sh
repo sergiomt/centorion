1646fedbfb244cde258feffb7339e59cdbc8f743
Sergio Montoro <sergiom@knowgate.com>
2017-06-11 08:40:09 +0100

    Cache Scala RPM

--- vagrant-setup/scala211.sh
+++ vagrant-setup/scala211.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+SETUP="/vagrant/vagrant-setup"
+
 echo "Installing Scala 2.11..."
-yum localinstall -y http://www.scala-lang.org/files/archive/scala-2.11.0.rpm
+if [ -f "$SETUP/cache/$2" ]
+	then
+	yum localinstall -y $SETUP/cache/scala-2.11.0.rpm
+else
+	yum localinstall -y http://www.scala-lang.org/files/archive/scala-2.11.0.rpm
+fi
