82b3a63ed81cec7983ebda319692e6c37d02ec33
Sergio Montoro <sergiom@knowgate.com>
2018-02-24 13:33:49 +0000

    Change Clocial path to workspace

--- vagrant-setup/clocial/redeploy.sh
+++ vagrant-setup/clocial/redeploy.sh
@@ -1,7 +1,7 @@
 echo Copying Clocial Classes
-cp -rvu /vagrant/clocial/clocial-web/target/classes/* /vagrant/clocial/clocial-web/target/clocial-web-1.0/WEB-INF/classes
+cp -rvu /vagrant/workspace/clocial/clocial-web/target/classes/* /vagrant/workspace/clocial/clocial-web/target/clocial-web-1.0/WEB-INF/classes
 echo Copying Clocial Resources
-cp -rvu /vagrant/clocial/clocial-web/src/resources/* /vagrant/clocial/clocial-web/target/clocial-web-1.0/WEB-INF/classes
+cp -rvu /vagrant/workspace/clocial/clocial-web/src/resources/* /vagrant/workspace/clocial/clocial-web/target/clocial-web-1.0/WEB-INF/classes
 echo Copying Clocial JSP Pages
-cp -rvu /vagrant/clocial/clocial-web/src/main/webapp/* /vagrant/clocial/clocial-web/target/clocial-web-1.0/
+cp -rvu /vagrant/workspace/clocial/clocial-web/src/main/webapp/* /vagrant/workspace/clocial/clocial-web/target/clocial-web-1.0/
 echo Done!
