2c390f068f9606f92724be72028ebb65c2d6f282
sergiomt <sergiom@knowgate.com>
2018-07-26 19:43:39 +0100

    Change clocial context

--- vagrant-setup/clocial-context.sh
+++ vagrant-setup/clocial-context.sh
@@ -6,11 +6,10 @@ then
 	if [ -d "/vagrant/workspace/clocial/clocial-web/target/clocial-web-1.0" ]
 	then
 			# METHOD 1
-			# Create context for clocial webapp
-			# and copy webapp configuration files
-			mkdir -p /usr/share/tomcat/conf/Catalina/localhost
-			cp $SETUP/clocial/conf/Catalina/localhost/clocial.xml /usr/share/tomcat/conf/Catalina/localhost
-			cp -r -f $SETUP/clocial/webapps/conf/WEB-INF/* /vagrant/clocial/clocial-web/target/clocial-web-1.0/WEB-INF
+			# Create context for clocial webapp and copy webapp configuration files
+			# mkdir -p /usr/share/tomcat/conf/Catalina/localhost
+			# cp $SETUP/clocial/conf/Catalina/localhost/clocial.xml /usr/share/tomcat/conf/Catalina/localhost
+			# cp -r -f $SETUP/clocial/webapps/conf/WEB-INF/* /vagrant/clocial/clocial-web/target/clocial-web-1.0/WEB-INF
 
 			# METHOD 2
 			# Create a symbolic link from Tomcat webapps to Maven target
@@ -19,6 +18,11 @@ then
 			# then
 			#		ln -s /vagrant/workspace/clocial/clocial-web/target/clocial-web-1.0 /usr/share/tomcat/webapps/clocial
 			#		cp -r -f $SETUP/clocial/webapps/conf/WEB-INF/* /usr/share/tomcat/webapps/clocial/WEB-INF
+			#	fi
+			
+			# METHOD 3
+			# Add shared libs to shared.loader
+
 	else
 			echo "Cannot find Clocial webapp target at /vagrant/workspace/clocial/clocial-web/target/"
 	fi
