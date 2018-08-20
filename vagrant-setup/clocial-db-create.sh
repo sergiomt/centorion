f6196c73acfd807c65524ffe3d9cca63919c87a4
sergiomt <sergiom@knowgate.com>
2018-08-20 08:20:45 +0100

    Upgrade Postgis to 2.4

--- vagrant-setup/clocial-db-create.sh
+++ vagrant-setup/clocial-db-create.sh
@@ -5,7 +5,8 @@ if getent passwd clocial > /dev/null 2>&1; then
 else
 	echo "Creating clocial user"
 	# Do not set any password for Linux user clocial
-	adduser clocial
+	adduser clocialexit
+	
 	su postgres -c "psql -c \"create role clocial with password 'clocial' superuser login;\""
 fi
 
@@ -15,11 +16,11 @@ su postgres -c "psql -c \"grant all privileges on database clocialdev to clocial
 
 # Create PostGIS 2.3 tables and functions in clocialdev database
 echo "Creating PostGIS spatial extensions at clocialdev database"
-su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.3/postgis.sql" > /dev/null
-su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.3/postgis_comments.sql" > /dev/null
-su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.3/spatial_ref_sys.sql" > /dev/null
-su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.3/topology.sql" > /dev/null
-su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.3/topology_comments.sql" > /dev/null
+su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.4/postgis.sql" > /dev/null
+su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.4/postgis_comments.sql" > /dev/null
+su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.4/spatial_ref_sys.sql" > /dev/null
+su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.4/topology.sql" > /dev/null
+su postgres -c "psql -d clocialdev -f /usr/pgsql-9.6/share/contrib/postgis-2.4/topology_comments.sql" > /dev/null
 
 echo "Creating openfire database"
 su postgres -c "psql -c \"create database openfire with owner=clocial\""
