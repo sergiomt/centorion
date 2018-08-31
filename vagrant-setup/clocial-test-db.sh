049939ecbf1dd63c071aba8618f0a08005c7dab0
sergiomt <sergiom@knowgate.com>
2018-08-31 17:39:01 +0100

    Change DB creation method from Java exec to Maven exec

--- vagrant-setup/clocial-test-db.sh
+++ vagrant-setup/clocial-test-db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PPWD=$PWD
 JAVA_HOME=/usr/java/latest
 SHARE=/usr/share
 SHARED=$SHARE/tomcat/shared/lib
@@ -7,7 +8,7 @@ M2REPO=/home/vagrant/.m2/repository
 WORKSPACE=/vagrant/workspace
 WEBINF=$WORKSPACE/clocial/clocial-web/target/clocial-web-1.0/WEB-INF/lib
 TARGET=$WORKSPACE/clocial/clocial-devel/target
-FATJAR=$WORKSPACE/clocial/clocial-web/target/clocial-webapp-jar-with-dependencies.jar
+SERVICES=$WORKSPACE/clocial/clocial-service/target/clocial-services-1.0.jar
 DEVEL=$WORKSPACE/clocial/clocial-devel/target/clocial-devel-1.0.jar
 BDBBUCKET=/opt/bdb/bucket
 
@@ -24,20 +25,31 @@ BDBBUCKET=/opt/bdb/bucket
 # fi
 
 # Uncomment this line for deleting all images from test bucket
-# echo Deleting images from test bucket
-# sudo $JAVA_HOME/bin/java -cp $FATJAR:$SHARED/*:$WEBINF/*:$M2REPO/junit/junit/4.11/junit-4.11.jar:$TARGET/test-classes -Djava.library.path=/usr/share/db-6.2.32/build_unix/.libs com.clocial.test.Blobs truncate
+echo Deleting images from test bucket
+# sudo $JAVA_HOME/bin/java -cp $SERVICES:$M2REPO/junit/junit/4.11/junit-4.11.jar:$TARGET/test-classes -Djava.library.path=/usr/share/db-6.2.32/build_unix/.libs com.clocial.test.Blobs truncate
+cd $WORKSPACE/clocial/clocial-devel
+mvn exec:java -P blobs -Dexec.args="truncate"
+cd $PPWD
 
 # Uncomment this line for deleting all tables and functions at the database before re-creating it
 echo Droping datamodel
-sudo $JAVA_HOME/bin/java -cp $DEVEL:$FATJAR:$SHARED/*:$WEBINF/* com.clocial.datamodel.ClocialDataModelManager /etc/clocial.cnf drop database
+cd $WORKSPACE/clocial/clocial-devel
+mvn exec:java -P database -Dexec.args="/etc/clocial.cnf drop database"
+cd $PPWD
+# sudo $JAVA_HOME/bin/java -cp $DEVEL:$SERVICES com.clocial.datamodel.ClocialDataModelManager /etc/clocial.cnf drop database
 
 # Create Clocial datamodel
 echo Creating datamodel
-sudo $JAVA_HOME/bin/java -cp $DEVEL:$FATJAR:$SHARED/*:$WEBINF/* com.clocial.datamodel.ClocialDataModelManager /etc/clocial.cnf create database
+cd $WORKSPACE/clocial/clocial-devel
+mvn exec:java -P database -Dexec.args="/etc/clocial.cnf create database"
+cd $PPWD
+# sudo $JAVA_HOME/bin/java -cp $DEVEL:$SERVICES com.clocial.datamodel.ClocialDataModelManager /etc/clocial.cnf create database
 
 # Populate the database specified at $TARGET/test-classes/com/clocial/test/testrdbms.cnf with test data
-# echo Populating database with test data
-# sudo $JAVA_HOME/bin/java -cp $DEVEL:$FATJAR:$SHARED/*:$WEBINF/*:$M2REPO/junit/junit/4.11/junit-4.11.jar:$TARGET/test-classes -Djava.library.path=/usr/share/db-6.2.32/build_unix/.libs com.clocial.users.UsersCreation
+echo Populating database with test data
+cd $WORKSPACE/clocial/clocial-devel
+mvn -Dtest=UsersCreation test
+cd $PPWD
 
 # Uncomment to create HBase tables for InMailService
 # if [ -d "/usr/share/hbase/conf" ]
