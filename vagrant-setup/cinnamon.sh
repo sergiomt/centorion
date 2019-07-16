34c9824ced6b2035ed5bd71b340f744d21c4029f
sergiomt <sergiom@knowgate.com>
2019-07-16 09:37:48 +0200

    Minor Cinnamon changes

--- vagrant-setup/cinnamon.sh
+++ vagrant-setup/cinnamon.sh
@@ -35,8 +35,6 @@ then
   # sudo yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
   # sudo yum -y install sublime-text
 
-	# yum -y groupinstall gnome-desktop
-
 	systemctl set-default graphical.target
 
 	# Change background
